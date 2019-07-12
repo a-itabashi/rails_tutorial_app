91ebebcc02825a896525edcb7f2116268ccc7119
Aguru itabashi <itabashi.a@gmail.com>
2019-07-12 12:38:31 +0900

    feat: signupのリンク生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'staticpages#home'
   get  '/help',    to: 'staticpages#help'
   get  '/about',   to: 'staticpages#about'
   get  '/contact', to: 'staticpages#contact'
+
+  get 'signup', to: 'users#new'
 end
