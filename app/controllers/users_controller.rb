8bc8f6cc651b785c637fa1043a6b8d3bc3303e4b
Aguru itabashi <itabashi.a@gmail.com>
2019-07-16 19:26:06 +0900

    feat: userのindex,edit,update実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[index edit update]
+  before_action :correct_user, only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -17,8 +24,35 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user, flash: {success: "Profile updated"}
+    else
+      render 'edit'
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def logged_in_user
+    unless logged_in? 
+      store_location
+      redirect_to login_path, flash: {danger: "Please log in."}
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
 end
