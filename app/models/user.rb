d18ac7cfbb7380ba192cd3768d9ca997b2be2f06
Aguru itabashi <itabashi.a@gmail.com>
2019-07-29 14:01:41 +0900

    feat: relationshipsモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
 
+  has_many :active_relationships, class_name: "Relationship", 
+              foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
 
   class << self
     # ハッシュ値を生成
@@ -83,6 +87,22 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーフォローを解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   private
 
